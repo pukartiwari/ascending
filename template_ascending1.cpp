084e16149a90da7793bf346aa7d35d2801fb7832
pukartiwari <pukartiwari0@gmail.com>
2022-04-20 14:09:20 +0545

    files upload

---
+++ template_ascending1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+template <typename T>
+T sort (T n)
+{
+	T a[30];
+	T i=0,j=0,t=0;
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			
+				if(a[i]>a[j])
+			{
+			
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+			}
+		}
+	}
+	cout<<"Ascending order of given input is :";
+		for(i=0;i<n;i++)
+		{
+		  cout<<a[i]<<endl;
+}
+}
+
+int main()
+{
+int n,result;
+cout<<"enter the number you want in a ascending order"<<endl;
+cin>>n;
+result=sort<int>(n);	
+return 0;	
+}
